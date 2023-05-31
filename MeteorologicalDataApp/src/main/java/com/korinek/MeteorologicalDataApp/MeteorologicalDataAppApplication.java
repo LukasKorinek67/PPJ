dd5f3373934ab97989969604650bda6d61bc4a4b
Lukas_Korinek <lukas.korinek@tul.cz>
2023-05-31 02:42:22 +0200

    Cassandra secure-connect-bundle JAR problem fix

--- MeteorologicalDataApp/src/main/java/com/korinek/MeteorologicalDataApp/MeteorologicalDataAppApplication.java
+++ MeteorologicalDataApp/src/main/java/com/korinek/MeteorologicalDataApp/MeteorologicalDataAppApplication.java
@@ -8,10 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 
 @SpringBootApplication
@@ -25,7 +33,28 @@ public class MeteorologicalDataAppApplication {
 
 	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-		Path bundle = astraProperties.getSecureConnectBundle().toPath();
-		return builder -> builder.withCloudSecureConnectBundle(bundle);
+		//Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		//return builder -> builder.withCloudSecureConnectBundle(bundle);
+
+		/*String bundlePath = astraProperties.getSecureConnectBundle();
+		Path bundle;
+		try {
+			Resource resource = new ClassPathResource(bundlePath);
+			File file = resource.getFile();
+			bundle = file.toPath();
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to load secure connect bundle", e);
+		}
+		return builder -> builder.withCloudSecureConnectBundle(bundle);*/
+
+		String bundlePath = astraProperties.getSecureConnectBundle();
+		Resource resource = new ClassPathResource(bundlePath);
+		try (InputStream inputStream = resource.getInputStream()) {
+			File tempFile = File.createTempFile("secure-connect", ".zip");
+			Files.copy(inputStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			return builder -> builder.withCloudSecureConnectBundle(tempFile.toPath());
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to load secure connect bundle", e);
+		}
 	}
 }
--- MeteorologicalDataApp/src/main/java/com/korinek/MeteorologicalDataApp/configuration/DataStaxAstraProperties.java
+++ MeteorologicalDataApp/src/main/java/com/korinek/MeteorologicalDataApp/configuration/DataStaxAstraProperties.java
@@ -1,18 +1,19 @@
 package com.korinek.MeteorologicalDataApp.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
 
 import java.io.File;
 
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
-    private File secureConnectBundle;
+    private String secureConnectBundle;
 
-    public File getSecureConnectBundle() {
+    public String getSecureConnectBundle() {
         return secureConnectBundle;
     }
 
-    public void setSecureConnectBundle(File secureConnectBundle) {
+    public void setSecureConnectBundle(String secureConnectBundle) {
         this.secureConnectBundle = secureConnectBundle;
     }
 }
